d427e38aa4dfa9ed46f71b7ddbee5d1cb1b182b0
jungtaeyong <ckck5050@naver.com>
2019-09-25 18:01:50 +0900

    week2 & week3

--- week2/beakjoon 9663.cpp
+++ week2/beakjoon 9663.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 using namespace std;
-//arr[row][col]가 아닌 arr[row]=col or arr[col]=row 로 표 
-int n,row[15],ans;
 
+//arr[row][col]가 아닌 arr[row]=col or arr[col]=row 로 표현 
+int n,row[15],ans;
 bool check(int now){
 	for(int j=0;j<now;j++){
 		//abs() : 절대값 구하는 함수 ->stdlib.h
 		if(row[j]==row[now] || abs(row[j]-row[now])==abs(j-now)){
-			return false;현 
+			return false;
 		}
 	}
 	return true;
@@ -27,11 +27,8 @@ void queen(int now){
 		}
 	}
 }
-
 int main(){
-	
 	cin>>n;
 	queen(0);
 	cout<<ans;
-	
 }
