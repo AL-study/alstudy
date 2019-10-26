845d012d921a2df1c5ad59f648461d3fe59ca2d5
Yewon Jeong <47112889+yuuil@users.noreply.github.com>
2019-10-26 12:43:00 +0900

    boj 1422 숫자의 신

--- week7/boj1422.cpp
+++ week7/boj1422.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
+bool cmp(string &str1, string &str2){
+  return (str1+str2) > (str2+str1);
+}
+
 int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  bool chk = false;
+  int K, N, high = 0;
+  cin>>K>>N;
+  string number[K];
+  for(int i=0; i<K; i++) {
+    cin>>number[i];
+    if(high < atoi(number[i].c_str())) high = atoi(number[i].c_str());
+  }
+  sort(number, number+K, cmp);
+  for(int i=0; i<K; i++) {
+    cout<<number[i];
+    if(atoi(number[i].c_str()) == high && !chk) {
+      chk = true;
+      for(int j=0; j< N-K; j++) {
+        cout<<number[i];
+      }
+    }
+  }
+  cout<<"\n";
   return 0;
 }
